73a5aaf242825953635b13e2574d039a8c910756
visualstudio <visualstudio@230-PC43>
2019-02-07 11:55:49 -0500

    Final Commit

--- C0745198_week05/js/main.js
+++ C0745198_week05/js/main.js
@@ -7,5 +7,13 @@ function myFunction(a, b) {
 function arrays() {
     var pens;
     pens = ["red", "orange", "green", "blue", "purple"];
+    markers = pens;
     console.log(pens);
+    console.log(pens[3]);
+
+    for (i = 0; i < pens.length; i++) {
+        alert(pens[i]);
+    }
+    alert("Length of arrays is " + i);
+    alert("Length of arrays is " + pens.length);
 }
