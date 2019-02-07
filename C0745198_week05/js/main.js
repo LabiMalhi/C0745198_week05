aa29540e3bd9fcdd85185752a921d7f571e98eaa
visualstudio <visualstudio@230-PC43>
2019-02-07 11:43:47 -0500

    11:45 Am Sync

--- C0745198_week05/js/main.js
+++ C0745198_week05/js/main.js
@@ -1,1 +1,11 @@
 ﻿// Your code here!
+function myFunction(a, b) {
+    return  a + b;
+}
+
+
+function arrays() {
+    var pens;
+    pens = ["red", "orange", "green", "blue", "purple"];
+    console.log(pens);
+}
